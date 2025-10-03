29fb4752f40aecfc78e9957f4f2e2f1bde2b54e8
neopoleon <jinyun.liu.jeff@gmail.com>
2025-10-02 20:42:58 -0700

    PyPI version update and upload script update (test pypi to pypi)

--- wheels/upload_to_pypi.sh
+++ wheels/upload_to_pypi.sh
@@ -4,4 +4,5 @@ set -e
 pip install --upgrade build
 pip install twine
 python -m build
-twine upload dist/* --verbose --repository-url https://test.pypi.org/legacy/ 
+# twine upload dist/* --verbose --repository-url https://test.pypi.org/legacy/ 
+twine upload dist/* --verbose
